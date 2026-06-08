549c042688af5a222d667c35495bb3a503a55060
Nathan Abramson <arista@abramsons.info>
2026-06-08 03:25:58 +0000

    scaffolded out other model elements

--- src/model.rs
+++ src/model.rs
@@ -8,20 +8,71 @@ pub type ModelResult<T> = Result<T, ModelError>;
 
 pub struct Model {
     pub files: ModelItems<File>,
+    pub file_items: ModelItems<FileItem>,
+    pub type_decls: ModelItems<TypeDecl>,
+    pub import_decls: ModelItems<ImportDecl>,
+    pub statements: ModelItems<Statement>,
+    pub empty_statements: ModelItems<EmptyStatement>,
+    pub expression_statements: ModelItems<ExpressionStatement>,
+    pub if_statements: ModelItems<IfStatement>,
+    pub while_statements: ModelItems<WhileStatement>,
+    pub return_statements: ModelItems<ReturnStatement>,
+    pub break_statements: ModelItems<BreakStatement>,
+    pub continue_statements: ModelItems<ContinueStatement>,
+    pub block_statements: ModelItems<BlockStatement>,
+    pub for_statements: ModelItems<ForStatement>,
+    pub switch_statements: ModelItems<SwitchStatement>,
+    pub var_decl_statements: ModelItems<VarDeclStatement>,
+    pub function_decl_statements: ModelItems<FunctionDeclStatement>,
+    pub labeled_statements: ModelItems<LabeledStatement>,
+    pub try_statements: ModelItems<TryStatement>,
+    pub expressions: ModelItems<Expression>,
+    pub comma_expressions: ModelItems<CommaExpression>,
+    pub ternary_expressions: ModelItems<TernaryExpression>,
+    pub binary_expressions: ModelItems<BinaryExpression>,
+    pub unary_expressions: ModelItems<UnaryExpression>,
+    pub boolean_literals: ModelItems<BooleanLiteral>,
+    pub null_literals: ModelItems<NullLiteral>,
+    pub string_literals: ModelItems<StringLiteral>,
     pub int_literals: ModelItems<IntLiteral>,
+    pub member_expressions: ModelItems<MemberExpression>,
+    pub identifier_expressions: ModelItems<IdentifierExpression>,
 }
 
 impl Model {
     pub fn new() -> Self {
         Self {
             files: ModelItems::new(ItemKind::File),
-            
+            file_items: ModelItems::new(ItemKind::FileItem),
+            type_decls: ModelItems::new(ItemKind::TypeDecl),
+            import_decls: ModelItems::new(ItemKind::ImportDecl),
+            statements: ModelItems::new(ItemKind::Statement),
+            empty_statements: ModelItems::new(ItemKind::EmptyStatement),
+            expression_statements: ModelItems::new(ItemKind::ExpressionStatement),
+            if_statements: ModelItems::new(ItemKind::IfStatement),
+            while_statements: ModelItems::new(ItemKind::WhileStatement),
+            return_statements: ModelItems::new(ItemKind::ReturnStatement),
+            break_statements: ModelItems::new(ItemKind::BreakStatement),
+            continue_statements: ModelItems::new(ItemKind::ContinueStatement),
+            block_statements: ModelItems::new(ItemKind::BlockStatement),
+            for_statements: ModelItems::new(ItemKind::ForStatement),
+            switch_statements: ModelItems::new(ItemKind::SwitchStatement),
+            var_decl_statements: ModelItems::new(ItemKind::VarDeclStatement),
+            function_decl_statements: ModelItems::new(ItemKind::FunctionDeclStatement),
+            labeled_statements: ModelItems::new(ItemKind::LabeledStatement),
+            try_statements: ModelItems::new(ItemKind::TryStatement),
+            expressions: ModelItems::new(ItemKind::Expression),
+            comma_expressions: ModelItems::new(ItemKind::CommaExpression),
+            ternary_expressions: ModelItems::new(ItemKind::TernaryExpression),
+            binary_expressions: ModelItems::new(ItemKind::BinaryExpression),
+            unary_expressions: ModelItems::new(ItemKind::UnaryExpression),
+            boolean_literals: ModelItems::new(ItemKind::BooleanLiteral),
+            null_literals: ModelItems::new(ItemKind::NullLiteral),
+            string_literals: ModelItems::new(ItemKind::StringLiteral),
             int_literals: ModelItems::new(ItemKind::IntLiteral),
+            member_expressions: ModelItems::new(ItemKind::MemberExpression),
+            identifier_expressions: ModelItems::new(ItemKind::IdentifierExpression),
         }
-    }
-
-    pub fn add_file(&mut self, item: File) -> ItemPtr<File> {
-        self.files.add(item)
     }
 }
 
@@ -54,8 +105,35 @@ impl<T> ModelItems<T> {
 #[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
 pub enum ItemKind {
     File,
-    
+    FileItem,
+    TypeDecl,
+    ImportDecl,
+    Statement,
+    EmptyStatement,
+    ExpressionStatement,
+    IfStatement,
+    WhileStatement,
+    ReturnStatement,
+    BreakStatement,
+    ContinueStatement,
+    BlockStatement,
+    ForStatement,
+    SwitchStatement,
+    VarDeclStatement,
+    FunctionDeclStatement,
+    LabeledStatement,
+    TryStatement,
+    Expression,
+    CommaExpression,
+    TernaryExpression,
+    BinaryExpression,
+    UnaryExpression,
+    BooleanLiteral,
+    NullLiteral,
+    StringLiteral,
     IntLiteral,
+    MemberExpression,
+    IdentifierExpression,
 }
 
 #[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
@@ -65,15 +143,195 @@ pub struct ItemPtr<T> {
     _marker: PhantomData<T>,
 }
 
-#[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
+#[derive(Debug)]
 pub struct File {
+    pub items: Vec<ItemPtr<FileItem>>
+}
+
+#[derive(Debug)]
+pub enum FileItem {
+    ImportDecl(ItemPtr<ImportDecl>),
+    TypeDecl(ItemPtr<TypeDecl>),
+    Statement(ItemPtr<Statement>),
+}
+
+#[derive(Debug)]
+pub struct TypeDecl {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct ImportDecl {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub enum Statement {
+    EmptyStatement(ItemPtr<EmptyStatement>),
+    ExpressionStatement(ItemPtr<ExpressionStatement>),
+    IfStatement(ItemPtr<IfStatement>),
+    WhileStatement(ItemPtr<WhileStatement>),
+    ReturnStatement(ItemPtr<ReturnStatement>),
+    BreakStatement(ItemPtr<BreakStatement>),
+    ContinueStatement(ItemPtr<ContinueStatement>),
+    BlockStatement(ItemPtr<BlockStatement>),
+    ForStatement(ItemPtr<ForStatement>),
+    SwitchStatement(ItemPtr<SwitchStatement>),
+    VarDeclStatement(ItemPtr<VarDeclStatement>),
+    FunctionDeclStatement(ItemPtr<FunctionDeclStatement>),
+    LabeledStatement(ItemPtr<LabeledStatement>),
+    TryStatement(ItemPtr<TryStatement>),
 }
 
 
-#[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
+#[derive(Debug)]
+pub struct EmptyStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct ExpressionStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct IfStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct WhileStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct ReturnStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct BreakStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct ContinueStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct BlockStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct ForStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct SwitchStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct VarDeclStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct FunctionDeclStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct LabeledStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct TryStatement {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub enum Expression {
+    CommaExpression(ItemPtr<CommaExpression>),
+    TernaryExpression(ItemPtr<TernaryExpression>),
+    BinaryExpression(ItemPtr<BinaryExpression>),
+    UnaryExpression(ItemPtr<UnaryExpression>),
+    BooleanLiteral(ItemPtr<BooleanLiteral>),
+    NullLiteral(ItemPtr<NullLiteral>),
+    StringLiteral(ItemPtr<StringLiteral>),
+    IntLiteral(ItemPtr<IntLiteral>),
+    MemberExpression(ItemPtr<MemberExpression>),
+    IdentifierExpression(ItemPtr<IdentifierExpression>),
+}
+
+#[derive(Debug)]
+pub struct CommaExpression {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct TernaryExpression {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct BinaryExpression {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct UnaryExpression {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct BooleanLiteral {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct NullLiteral {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct StringLiteral {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
 pub struct IntLiteral {
-    value: u32,
+    // FIXME - implement this
 }
+
+#[derive(Debug)]
+pub struct MemberExpression {
+    // FIXME - implement this
+}
+
+#[derive(Debug)]
+pub struct IdentifierExpression {
+    // FIXME - implement this
+}
+
+
+
+
+
+
+
+
+
+
+
+// #[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
+// pub struct IntLiteral {
+//     value: u32,
+// }
 
 // #[derive(Eq, Hash, PartialEq, Clone, Copy)]
 // pub struct Id {
--- src/model_builder.rs
+++ src/model_builder.rs
@@ -1,10 +1,11 @@
 use crate::ast;
 use crate::model;
+use crate::peg_parser::Parsed;
 
-pub fn ast_to_model(ast: &ast::File) -> model::Model {
+pub fn ast_to_model(ast: &Parsed<ast::File>) -> model::Model {
     let mut model = model::Model::new();
     let mut mb = ModelBuilder::new(&mut model);
-    let _ = mb.build_file(ast);
+//    let _ = mb.build_file(ast);
     model
 }
 
@@ -19,8 +20,20 @@ impl<'a> ModelBuilder<'a> {
         }
     }
 
-    pub fn build_file(&mut self, ast: &ast::File) -> model::ItemPtr<model::File> {
-        self.model.add_file(model::File {
-        })
-    }
+    // pub fn build_file(&mut self, ast: &Parsed<ast::File>) -> model::ItemPtr<model::File> {
+    //     self.model.files.add(model::File {
+    //         items: ast.value.items.iter().map(|v| self.build_file_item(v)),
+    //     })
+    // }
+
+    // pub fn build_file_item(&mut self, ast: &Parsed<ast::FileItem>) -> model::ItemPtr<model::FileItem> {
+    //     match ast {
+    //         ast::FileItem::ImportDecl(v) => self.model.file_items.add(model::FileItem::ImportDecl(self.build_import_decl(v)))
+    //     }
+    // }
+
+    // pub fn build_import_decl(&mut self, ast: &Parsed<ast::ImportDecl>) -> model::ItemPtr<model::ImportDecl> {
+    //     self.model.import_decls.add(model::ImportDecl {
+    //     })
+    // }
 }
